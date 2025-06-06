1db38aae81941ae863b53abd18196a16a58390b8
MATHEUS ALVES DO NASCIMENTO <71792132+MatheusAlves65@users.noreply.github.com>
2025-06-05 21:39:32 -0300

    Delete Firmware /ADC_buffer directory

--- Firmware /ADC_buffer/main/global.c
+++
@@ -1,8 +0,0 @@
-#include "global.h"
-
-// Definindo a variável global para armazenar as medições do ADC
-Medicoes_ADC Tensao;
-Medicoes_ADC Corrente;
-
-// Definição da variável global
-bool buffer_completo = false;
